22bd8289b006650dfb47b8590431ba8e33ff12c0
Artur <flartur95@gmail.com>
2020-06-06 01:55:00 -0300

    Ajuste config name

--- ticket-system/resources/views/welcome.blade.php
+++ ticket-system/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    {{ config('app.name'); }}
+                    {{ config('app.name') }}
                 </div>
 
                 <div class="links">
